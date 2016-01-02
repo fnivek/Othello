b48d0d3ed7843bce5ffdce1055e50e29d157dc23
Kevin French <fnivek@gmail.com>
2016-01-02 16:12:41 -0500

    GRAPHICS: Draw board

--- Othello.h
+++ Othello.h
@@ -2,6 +2,8 @@
 #define OTHELLO_H
 
 #include <QMainWindow>
+#include <QPainter>
+#include <QVBoxLayout>
 
 class Othello : public QMainWindow
 {
@@ -10,6 +12,19 @@ class Othello : public QMainWindow
 public:
     Othello(QWidget *parent = 0);
     ~Othello();
+
+private:
+    // Graphics
+    QPen pen_;
+    QBrush brush_;
+    //QPixmap pixmap_;
+
+    // Game vars
+    unsigned short int board_size_ = 8;
+
+protected:
+    // Graphics
+    void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
 };
 
 #endif // OTHELLO_H
